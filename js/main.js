fc66691283e16154cb20025a4f2ab0d153d55765
Tolihama <fra070@gmail.com>
2021-11-15 14:06:23 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        todos: [
+            {
+                text: 'Todo 1',
+                completed: false,
+            },
+            {
+                text: 'Todo 2',
+                completed: true,
+            },
+            {
+                text: 'Todo 3',
+                completed: false,
+            },
+        ],
+    },
 });
